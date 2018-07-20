fbc6fbe273fb5f7a6bbdbdd94b513a65de34ab8e
莫聪 <qiuwei.cw@alibaba-inc.com>
2018-07-20 13:46:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
-#mkdir -p  experiments/fixed/0/pretrain_pos/model/
-python main.py --config configs/fixed/lstmcrf_pos.config | tee experiments/fixed/0/pretrain_pos/model/log
+cd data 
+git pull
 
-#mkdir -p  experiments/unfixed/0/pretrain_pos/model/
-python main.py --config configs/unfixed/lstmcrf_pos.config | tee experiments/unfixed/0/pretrain_pos/model/log
+cd ..
+mkdir -p  experiments/fixed/0/expr1/
+python main.py --config configs/fixed/expr1.config > experiments/fixed/0/expr1/log &
 
-mkdir -p  experiments/fixed/0/pretrain_pos/expr2/
-python main.py --config configs/fixed/expr2.config | tee experiments/fixed/0/pretrain_pos/expr2/log
+mkdir -p  experiments/unfixed/0/expr1/
+python main.py --config configs/unfixed/expr1.config > experiments/unfixed/0/expr1/log &
 
-mkdir -p  experiments/unfixed/0/pretrain_pos/expr2/
-python main.py --config configs/unfixed/expr2.config | tee experiments/unfixed/0/pretrain_pos/expr2/log
+#mkdir -p  experiments/fixed/0/expr2/
+#python main.py --config configs/fixed/expr2.config > experiments/fixed/0/expr2/log &
+
+#mkdir -p  experiments/unfixed/0/expr2/
+#python main.py --config configs/unfixed/expr2.config > tee experiments/unfixed/0/expr2/log &
