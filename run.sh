f2f8e909c416e5fd73e1d614e7d7a45a48cc544d
莫聪 <qiuwei.cw@alibaba-inc.com>
2018-07-20 12:47:23 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #mkdir -p  experiments/fixed/0/pretrain_pos/model/
-#python main.py --config configs/fixed/lstmcrf_pos.config | tee experiments/fixed/0/pretrain_pos/model/log
+python main.py --config configs/fixed/lstmcrf_pos.config | tee experiments/fixed/0/pretrain_pos/model/log
 
-mkdir -p  experiments/unfixed/0/pretrain_pos/model/
+#mkdir -p  experiments/unfixed/0/pretrain_pos/model/
 python main.py --config configs/unfixed/lstmcrf_pos.config | tee experiments/unfixed/0/pretrain_pos/model/log
+
+mkdir -p  experiments/fixed/0/pretrain_pos/expr2/
+python main.py --config configs/fixed/expr2.config | tee experiments/fixed/0/pretrain_pos/expr2/log
+
+mkdir -p  experiments/unfixed/0/pretrain_pos/expr2/
+python main.py --config configs/unfixed/expr2.config | tee experiments/unfixed/0/pretrain_pos/expr2/log
