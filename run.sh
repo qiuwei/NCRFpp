292ee424a886cd6f2c45761798b396d42f11404b
莫聪 <qiuwei.cw@alibaba-inc.com>
2018-07-20 14:57:55 +0800

    Add expr3-> without pos info

--- run.sh
+++ run.sh
@@ -9,7 +9,13 @@ mkdir -p  experiments/unfixed/0/expr1/
 #python main.py --config configs/unfixed/expr1.config > experiments/unfixed/0/expr1/log &
 
 mkdir -p  experiments/fixed/0/expr2/
-python main.py --config configs/fixed/expr2.config > experiments/fixed/0/expr2/log &
+#python main.py --config configs/fixed/expr2.config > experiments/fixed/0/expr2/log &
 
 mkdir -p  experiments/unfixed/0/expr2/
-python main.py --config configs/unfixed/expr2.config > experiments/unfixed/0/expr2/log &
+#python main.py --config configs/unfixed/expr2.config > experiments/unfixed/0/expr2/log &
+
+mkdir -p  experiments/fixed/0/expr3/
+python main.py --config configs/fixed/expr3.config > experiments/fixed/0/expr3/log &
+
+mkdir -p  experiments/unfixed/0/expr3/
+python main.py --config configs/unfixed/expr2.config > experiments/unfixed/0/expr3/log &
