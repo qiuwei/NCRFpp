d3ba8c6b9a174319adedc2b4f346cbde848e4c56
莫聪 <qiuwei.cw@alibaba-inc.com>
2018-07-21 15:55:04 +0800

    Expr on 4

--- run.sh
+++ run.sh
@@ -2,11 +2,19 @@ cd data
 git pull
 
 cd ..
-python main.py --config configs/fixed/expr1.config &
+python main.py --config configs/fixed/0/expr1.config &
 
-python main.py --config configs/fixed/expr2.config & 
+python main.py --config configs/fixed/0/expr2.config & 
 
-python main.py --config configs/fixed/expr3.config &
+python main.py --config configs/fixed/0/expr3.config &
 
-python main.py --config configs/fixed/expr4.config &
+python main.py --config configs/fixed/0/expr4.config &
 
+
+python main.py --config configs/fixed/1/expr1.config &
+
+python main.py --config configs/fixed/1/expr2.config & 
+
+python main.py --config configs/fixed/1/expr3.config &
+
+python main.py --config configs/fixed/1/expr4.config &
