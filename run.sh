cf174b6217daba880d620cd097176f3a818d86dc
莫聪 <qiuwei.cw@alibaba-inc.com>
2018-07-21 13:42:18 +0800

    Add experiment 4 trying sgd

--- run.sh
+++ run.sh
@@ -2,12 +2,11 @@ cd data
 git pull
 
 cd ..
-mkdir -p  experiments/fixed/0/expr1/
-python main.py --config configs/fixed/expr1.config > experiments/fixed/0/expr1/log &
+python main.py --config configs/fixed/expr1.config &
 
-mkdir -p  experiments/fixed/0/expr2/
-python main.py --config configs/fixed/expr2.config > experiments/fixed/0/expr2/log &
+python main.py --config configs/fixed/expr2.config & 
 
-mkdir -p  experiments/fixed/0/expr3/
-python main.py --config configs/fixed/expr3.config > experiments/fixed/0/expr3/log &
+python main.py --config configs/fixed/expr3.config &
+
+python main.py --config configs/fixed/expr4.config &
 
